85abdc41c581900ba62a1a5ad4ef4f984cfa266a
nottimnik <timnikofficial@gmail.com>
2023-02-12 04:34:11 -0500

    Added navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <div className="App">
+    <h1>Hello</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
